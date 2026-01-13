2d48cbd0e1ff01e14ee90418364c5e57b8f743f8
Henil Darji <henildarji001@gmail.com>
2026-01-13 17:07:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const targetDate = new Date("Dec 31, 2026 00:00:00").getTime();
+
+setInterval(() => {
+    const now = new Date().getTime();
+    const diff = targetDate - now;
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((diff / (1000 * 60)) % 60);
+    const seconds = Math.floor((diff / 1000) % 60);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+}, 1000);
